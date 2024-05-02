ebca5fe86226a2a19561b4ae3060287cd508d053
ameralnas <amer.live477@gmail.com>
2024-05-02 20:57:43 +0300

    First two

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""A file to add some floats lol"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Args:
+        a: float
+        b: float
+
+    Return:
+        float
+    """
+    return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""A file to add some strings"""
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Args:
+        str1: string
+        str2: string
+
+    Return:
+        string
+    """
+    return str1 + str2
